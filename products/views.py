b2aaff933451a5df1af327f40dc5db7ed654a80d
athouba99 <athooba99@gmail.com>
2022-10-13 02:41:27 +0300

    still

--- products/views.py
+++ products/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def get_home(request):
     return HttpResponse(f"<h1> Welcome </h1>")
+
+
+def get_product(request, product_id):
+    product = product.object.get(id=product_id)
+    return HttpResponse(product)
