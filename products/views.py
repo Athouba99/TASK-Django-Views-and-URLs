31f6f5c3c100bf4d1161755db6968890dbb28cda
athouba99 <athooba99@gmail.com>
2022-10-13 19:35:38 +0300

    views&URLs done

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def get_home(request):
@@ -7,5 +8,11 @@ def get_home(request):
 
 
 def get_product(request, product_id):
-    product = product.object.get(id=product_id)
-    return HttpResponse(product)
+    product = Product.objects.get(id=product_id)
+    return HttpResponse(
+        f"""
+        <h1> {product.name}</h1>
+        <h2> {product.price}</h2>
+        
+        """,
+    )
